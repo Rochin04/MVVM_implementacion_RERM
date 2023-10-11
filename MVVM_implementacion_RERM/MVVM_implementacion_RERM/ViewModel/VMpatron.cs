61c638a983ab4681d8b4c874847ec9434a245cd1
Rochin <emirochi22@gmail.com>
2023-10-11 09:11:05 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_RERM/MVVM_implementacion_RERM/ViewModel/VMpatron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_RERM.ViewModel
+{
+    public class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        #endregion
+        #region OBJETO
+        public string Texto
+        {
+            get{ return _Texto; }
+            set{ SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
